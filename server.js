5671000ada4acbdc1033bafaa763a15e5fcbcb3b
miss-gif <svx327@naver.com>
2024-11-03 05:58:39 +0900

    Docs: 미들웨어 정리

--- server.js
+++ server.js
@@ -17,6 +17,14 @@ const User = [
 
 const app = express();
 
+app.use((req, res, next) => {
+  const start = Date.now();
+  console.log(`start: ${req.method} ${req.url} `);
+  next();
+  const diffTime = Date.now() - start;
+  console.log(`end: ${req.method} ${req.url} ${diffTime}ms`);
+});
+
 app.get("/user/:id", function (req, res) {
   const id = Number(req.params.id);
   const user = User[id - 1];
